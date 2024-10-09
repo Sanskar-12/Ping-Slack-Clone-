88b5a4e2875c9836f167951c8071c8abb477f4fd
Sanskar-12 <sanskarv2004@gmail.com>
2024-10-09 18:06:49 +0530

    thread almost done

--- src/features/messages/components/thread.tsx
+++ src/features/messages/components/thread.tsx
@@ -2,6 +2,10 @@ import { Button } from "@/components/ui/button";
 import { Id } from "../../../../convex/_generated/dataModel";
 import { AlertTriangle, Loader, XIcon } from "lucide-react";
 import { useGetMessageById } from "../api/use-get-message-by-id";
+import Message from "@/components/message";
+import { useCurrentMember } from "@/features/members/api/use-current-members";
+import { useWorkSpaceId } from "@/features/workspaces/api/use-workspace-id";
+import { useState } from "react";
 
 interface ThreadProps {
   messageId: Id<"messages">;
@@ -9,14 +13,28 @@ interface ThreadProps {
 }
 
 const Thread = ({ messageId, onClose }: ThreadProps) => {
+  const workspaceId = useWorkSpaceId();
+
+  const [editingId, setEditingId] = useState<Id<"messages"> | null>(null);
+
+  const { data: currentMember } = useCurrentMember({ workspaceId });
   const { data: message, isLoading: isLoadingMessage } = useGetMessageById({
     messageId,
   });
 
   if (isLoadingMessage) {
     return (
-      <div className="flex h-full items-center justify-center">
-        <Loader className="size-5 animate-spin text-muted-foreground" />
+      <div className="h-full flex flex-col">
+        <div className="h-[49px] flex justify-between items-center px-4 border-b">
+          <p className="text-lg font-bold">Thread</p>
+          <Button onClick={onClose} size={"iconSm"} variant={"ghost"}>
+            <XIcon className="size-5 stroke-[1.5]" />
+          </Button>
+        </div>
+        <div className="flex flex-col gap-y-2 h-full items-center justify-center">
+          <Loader className="animate-spin size-5  text-muted-foreground" />
+          <p className="text-sm text-muted-foreground">Message not found</p>
+        </div>
       </div>
     );
   }
@@ -44,9 +62,21 @@ const Thread = ({ messageId, onClose }: ThreadProps) => {
           <XIcon className="size-5 stroke-[1.5]" />
         </Button>
       </div>
-      {
-        JSON.stringify(message)
-      }
+      <Message
+        hideThreadButton
+        memberId={message!.memberId}
+        authorName={message?.user.name}
+        authorImage={message?.user.image}
+        isAuthor={currentMember?._id === message?.memberId}
+        body={message!.body}
+        image={message?.image}
+        createdAt={message!._creationTime}
+        updatedAt={message!.updatedAt}
+        id={message!._id}
+        reactions={message!.reactions}
+        isEditing={editingId === message?._id}
+        setEditingId={setEditingId}
+      />
     </div>
   );
 };
